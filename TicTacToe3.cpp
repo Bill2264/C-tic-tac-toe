372a88e33fe3ea5bbd2249e663feed12e0d8cb87
Riokelly20 <150245271+Riokelly20@users.noreply.github.com>
2025-04-07 13:21:05 +0100

    Add files via upload

---
+++ TicTacToe3.cpp
@@ -0,0 +1,156 @@
+﻿//Rio Kelly
+//Group Project
+//28/0/2025
+
+#include<iostream>
+
+using namespace std;
+
+char board[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int win()
+{
+	if (board[1] == board[2] && board[2] == board[3])
+	{
+		return 1;
+	}
+	else if (board[4] == board[5] && board[5] == board[6])
+	{
+		return 1;
+	}
+	else if (board[7] == board[8] && board[8] == board[9])
+	{
+		return 1;
+	}
+	else if (board[1] == board[4] && board[4] == board[7])
+	{
+		return 1;
+	}
+	else if (board[2] == board[5] && board[5] == board[8])
+	{
+		return 1;
+	}
+	else if (board[3] == board[6] && board[6] == board[9])
+	{
+		return 1;
+	}
+	else if (board[1] == board[5] && board[5] == board[9])
+	{
+		return 1;
+	}
+	else if (board[3] == board[5] && board[5] == board[7])
+	{
+		return 1;
+	}
+	else if (board[1] != '1' && board[2] == board[3] != '3' && board[4] != '4' && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[5] != '8' && board[9] != '9')
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+void square()
+{
+	system("cls");
+	cout << "\n\n Tic Tac Toe Game \n\n";
+
+	cout << "Player 1(x)  -  Player 2(o)" << endl << endl;
+	cout << endl;
+
+	cout << "       |       |       " << endl;
+	cout << " " << board[1] << "     |    " << board[2] << "  |     " << board[3] << endl;
+
+	cout << "_______|_______|_______" << endl;
+	cout << "       |       |       " << endl;
+
+	cout << " " << board[4] << "     |    " << board[5] << "  |     " << board[6] << endl;
+
+	cout << "_______|_______|_______" << endl;
+	cout << "       |       |       " << endl;
+
+	cout << " " << board[7] << "     |    " << board[8] << "  |     " << board[9] << endl;
+
+	cout << "       |       |       " << endl;
+}
+
+int main()
+{
+	int player = 1, i, choice;
+
+	char mark;
+
+	do
+	{
+		square();
+		player = (player % 2) ? 1 : 2;
+
+		cout << "Player " << player << " Enter the number: ";
+		cin >> choice;
+
+		mark = (player == 1) ? 'x' : 'o';
+
+		if (choice == 1 && board[1] == '1')
+		{
+			board[1] - mark;
+		}
+		else if (choice == 2 && board[2] == '2')
+		{
+			board[2] - mark;
+		}
+		else if (choice == 3 && board[3] == '3')
+		{
+			board[3] - mark;
+		}
+		else if (choice == 4 && board[2] == '4')
+		{
+			board[4] - mark;
+		}
+		else if (choice == 5 && board[5] == '5')
+		{
+			board[5] - mark;
+		}
+		else if (choice == 6 && board[6] == '6')
+		{
+			board[6] - mark;
+		}
+		else if (choice == 7 && board[7] == '7')
+		{
+			board[7] - mark;
+		}
+		else if (choice == 8 && board[8] == '8')
+		{
+			board[8] - mark;
+		}
+		else if (choice == 9 && board[9] == '9')
+		{
+			board[9] - mark;
+		}
+		else
+		{
+			cout << "INVALID MOVE!";
+
+			player--;
+			cin.ignore();
+			cin.get();
+		}
+
+		i = win();
+		player++;
+	} while (i == -1);
+	square();
+	if (i == 1)
+	{
+		cout << "\aCONGRATULATIONS PLAYER " << --player << " WINS!";
+	}
+	else
+	{
+		cout << "\aGAME DRAW!";
+	}
+	cin.ignore();
+	cin.get();
+
+	return 0;
+}
